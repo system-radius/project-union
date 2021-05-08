7f16fb6f2d1a0a19f4484a881d305fac9bd088b1
Darius Orias <radius.sairo@gmail.com>
2021-05-09 04:01:01 +0800

    feat: Add index convertion to properly retrieve values from the field array, to act as constraint to the tapping place for the divider.

--- Assets/GridController.cs
+++ Assets/GridController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class GridController : MonoBehaviour
 {
+    public const int SIZE_X = 24;
+
+    public const int SIZE_Y = 48;
+
+    private int[,] field = new int[SIZE_X + 1, SIZE_Y + 1];
+
+
     // The Divider prefab to be created.
     public GameObject divider;
 
@@ -11,16 +18,66 @@ public class GridController : MonoBehaviour
     void Awake()
     {
         SpawnDivider();
+        InitializeField();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SpawnDivider()
     {
         Instantiate(divider);
+    }
+
+    void InitializeField()
+    {
+        for (int x = 0; x <= SIZE_X; x++)
+        {
+            for (int y = 0; y <= SIZE_Y; y++)
+            {
+                field[x, y] = 0;
+            }
+        }
+    }
+
+    int[,] GetField()
+    {
+        return field;
+    }
+
+    public Vector2 UnitToExact(float x, float y)
+    {
+        // Convert x and y to their nearest whole number.
+        int cX = (int)(x + 0.5f);
+        int cY = (int)(y + 0.5f);
+
+        return new Vector2(cX, cY);
+    }
+
+    public Vector2 UnitToGridPoint(float x, float y)
+    {
+        // Convert x and y to their nearest whole number.
+        Vector2 exactCoords = UnitToExact(x, y);
+
+        // Adjust the coordinates to fit the field array.
+        int cX = (int)(exactCoords.x + (SIZE_X / 2));
+        int cY = (int)(exactCoords.y + (SIZE_Y / 2));
+        
+        return new Vector2(cX, cY);
+    }
+
+    public int GetGridValue(float x, float y)
+    {
+        Vector2 coords = UnitToGridPoint(x, y);
+
+        if (coords.x < 0 || coords.x > SIZE_X || coords.y < 0 || coords.y > SIZE_Y)
+        {
+            return -1;
+        }
+
+        return field[(int)coords.x, (int)coords.y];
     }
 }
